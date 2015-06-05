dbdba715991e6c093b8cacef1550be9a2981add5
Rafael <rafi.93.zgz@gmail.com>
2015-06-05 13:17:12 +0200

    vista

--- resources/views/control/assistencies/entrada.php
+++ resources/views/control/assistencies/entrada.php
@@ -1,66 +1,98 @@
 <!DOCTYPE html>
 <html lang="es">
-    <head>
-        <title>test</title>
-        <meta charset="UTF-8">
-        <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
-    </head>
-    <body>
-    <div name="dynamicContent">
-        <p id="result">Resultat</p>
-        <input type="text"/>
+<head>
+    <title>test</title>
+    <meta charset="UTF-8">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.3.min.js"></script>
+</head>
+
+
+<body id="bodycolor" style="background-color: #add8e6">
+<div id="result" name="dynamicContent" >
+
+    <div style="width: 35%;
+                height: 10%;
+                margin: auto;
+                position: absolute;
+                top: 0; left: -200px; bottom: 0; right: 0;
+                font-size: 700%;
+                ;
+                "
+         id="result">
+        Esperant qr...
     </div>
-    <script type="text/javascript">
-        var array;
-        function enviar(array){
-            var postassistencia = array;
-            $.ajax({
-                type: "POST",
-                url: '<?=url('api/control/assistencies');?>',
-                data: { array:postassistencia },
-                success: function(data) {
-                    if(data == 'ENTRADA'){
+
+</div>
+</body>
+
+<script type="text/javascript">
+    var array;
+    function enviar(array) {
+        var postassistencia = array;
+        $.ajax({
+            type: "POST",
+            url: '<?=url('api/control/assistencies');?>',
+            data: {array: postassistencia},
+            success: function (data) {
+                if (data == 'ENTRADA') {
+                    console.log(data);
+                    var text = '<div style="width: 30%; height: 10%; margin: auto; position: absolute; top: 0; left: -200px; bottom: 0; right: 0; font-size: 700%;" id="result">';
+                    text = text + data;
+                    text = text + '</div>';
+                    $("#result").html(text);
+                    $("#bodycolor").css('background-color', '#98FB98');
+                } else {
+                    if (data == 'SORTIDA') {
                         console.log(data);
-                        $("#result").text(data);
+                        var text = '<div style="width: 30%; height: 10%; margin: auto; position: absolute; top: 0; left: -200px; bottom: 0; right: 0; font-size: 700%;" id="result">';
+                        text = text + data;
+                        text = text + '</div>';
+                        $("#result").html(text);
+                        $("#bodycolor").css('background-color', '#98FB98');
                     } else {
-                        if(data == 'SORTIDA'){
-                            console.log(data);
-                            $("#result").text(data);
-                        }else{
-                            console.log(data);
-                            $("#result").text(data);
-                        }
+                        console.log(data);
+                        var text = '<div style="width: 40%; height: 10%; margin: auto; position: absolute; top: 0; left: -200px; bottom: 0; right: 0; font-size: 700%;" id="result">';
+                        text = text + data;
+                        text = text + '</div>';
+                        $("#result").html(text);
+                        $("#bodycolor").css('background-color', '#FFAEB0');
                     }
                 }
-            });
-        }
-        function doKeyDown(e){
-            console.log("Llegueix al clickar una tecla");
-             console.log(e);
-             console.log(e.key);
-             console.log(e.keyCode);
-
-            if(e.keyCode == "13"){
-                console.log("array definitivo");
-                console.log(array);
-
-                enviar(array);
-
-                array = "";
-                console.log("array limpio");
-                console.log(array);
-            }else{
-                array += e.key;
-                console.log(e.key);
+                setTimeout(function(){
+                    $("#bodycolor").css('background-color', '#add8e6');
+                    text = '<div style="width: 35%; height: 10%; margin: auto; position: absolute; top: 0; left: -200px; bottom: 0; right: 0; font-size: 700%;" id="result">Esperant qr...</div>';
+                    $("#result").html(text);
+                }, 1500);
             }
-        }
+        });
+    }
+    function doKeyDown(e) {
+        console.log("Llegueix al clickar una tecla");
+        console.log(e);
+        console.log(e.key);
+        console.log(e.keyCode);
 
-        function onLoadPage(){
-            console.log("Entra al carregar la pagina");
-            array = '';
-            window.addEventListener( "keypress", doKeyDown);
+        if (e.keyCode == "13") {
+            console.log("array definitivo");
+            console.log(array);
+
+            enviar(array);
+
+            array = "";
+            console.log("array limpio");
+            console.log(array);
+        } else {
+            array += e.key;
+            console.log(e.key);
         }
-        window.addEventListener( "DOMContentLoaded", onLoadPage);
-    </script>
-    </body>
+    }
+
+    function onLoadPage() {
+        console.log("Entra al carregar la pagina");
+        array = '';
+        window.addEventListener("keypress", doKeyDown);
+    }
+    window.addEventListener("DOMContentLoaded", onLoadPage);
+</script>
+
 </html>
